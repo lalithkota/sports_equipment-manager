e15d3047767cd3a1cab56b69288c54110fdb8bbd
Lalith Kota <AnanthaSurya.Lalith@iiitb.org>
2020-05-27 13:35:15 +0530

    changes v4

--- sportsEquipment/old_tasks.py
+++ sportsEquipment/old_tasks.py
@@ -19,7 +19,6 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from rest_framework import viewsets
 from .serializers import *
 from django.core.mail import EmailMessage
-import time
 
 #celery -A sportsroom worker -l info
 #celery -A sportsroom beat -l info
@@ -39,17 +38,15 @@ def utcToIst(lstRequest):
 
 	return lstRequest
 
-@periodic_task(run_every=(crontab(minute="*/15")), name="some_task", ignore_result=True)
+@periodic_task(run_every=(crontab(minute="*/1")), name="some_task", ignore_result=True)
 def some_task():
-	#print("Updating Penalty")
-
+	print("Updating Penalty")
+	
 	#processing penalty
 	lstProcessedRequest = list(EquipmentRequest.objects.filter(reqStatus__in = [1]).order_by('-dtOfRequest'))
 	lstProcessedRequest = utcToIst(lstProcessedRequest)
-	cnt=0
 	for i in lstProcessedRequest:
-		print(cnt)
-		cnt+=1
+		print(i.user)
 		usr = i.user
 		#print("fafafafasafsafs      ",usr)
 		userProfile = UserProfileInfo.objects.get(user= usr)
@@ -64,18 +61,15 @@ def some_task():
 		if (delta.days > 0):
 			penaltyAmount = dailyPenalty * delta.days
 		userProfile.totalPenalty += penaltyAmount
-		#print(usr.email)
+		print(usr.email)
 		if(userProfile.totalPenalty>0):
-			mail = EmailMessage('Sports Room Penalty', 'This is to notify that your penalty now has reached:'+
+			mail = EmailMessage('Sports Room Penalty', 'This is to notify that your penalty now is:'+
 				str(userProfile.totalPenalty),to=[usr.email])
 			mail.send()
 		insertOrUpdate(userProfile)
-	print("saaas")
-	# time.sleep(30)
-	print("gggggg")
-	#clearing ground
+
+	#deleting all ground records
 	currentTime = datetime.today().astimezone(timezone('Asia/Kolkata'))
-	#print(currentTime)
 	if(currentTime.hour==5 and currentTime.minute>=0 and currentTime.minute<=10):
 		all_grounds = list(Ground.objects.order_by('gId'))
 		for i in all_grounds:
@@ -83,35 +77,3 @@ def some_task():
 			i.who_booked = ";"
 			insertOrUpdate(i)
 
-	#clearing not verified users
-	#print("dafsdaf")
-	unverified = list(UserProfileInfo.objects.filter(is_verified = 0))
-	for i in unverified:
-		print(i.user.username)
-		i.user.delete()
-	UserProfileInfo.objects.filter(is_verified = 0).delete()
-	# UserProfile = UserProfileInfo.objects.get(user=request.user)
-	# Equipments.objects.filter(eqpId=pk).delete()
-	# items = Equipments.objects.all()
-	# bin/logstash -f logstash-simple.conf
-
-
-	#sending email to admin every month notifying penalty of all users
-	context = list(UserProfileInfo.objects.order_by('totalPenalty'))
-	lst = ""
-	for i in context:
-		t = i.user.username + "	  " + str(i.totalPenalty) + "\n"
-		lst+= t
-	mail2 = EmailMessage('User Penalty List', lst ,to=["kartik.gupta@iiitb.org"])
-	mail2.send()
-
-	# time.sleep(60)
-	print("jjjjjj")
-	#sending email to admin every month notifying current sports inventory
-	context = list(Equipments.objects.order_by('-eqpId'))
-	lst = ""
-	for i in context:
-		t = i.eqpName + "	  " + str(i.eqpQuantity) + "\n"
-		lst+= t
-	mail3 = EmailMessage('Current Sports Inventory', lst ,to=["kartik.gupta@iiitb.org"])
-	mail3.send()
